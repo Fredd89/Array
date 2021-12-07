a21bb5b209680e2a604df785c402e03c2b21c37f
Fredd89 <ficofedebla@gmail.com>
2021-12-07 09:53:32 +0100

    Listview added, not working

--- app/src/main/java/com/example/array/MainActivity.java
+++ app/src/main/java/com/example/array/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.array;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String nazioni[] = {"Italia", "Germania", "Olanda", "Stati Uniti"};
+    ListView countryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countryList = (ListView) findViewById(R.id.countryList);
+        ArrayAdapter<String> countryAarrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_main, nazioni);
+        countryList.setAdapter(countryAarrayAdapter);
+
     }
 }
