6a3a670885179cc282f5b07af1e9f7cfd82179e5
Fred <ficofedebla@gmail.com>
2021-12-13 17:03:21 +0100

    Added third activity, not working

--- app/src/main/java/com/example/array/Listview.java
+++ app/src/main/java/com/example/array/Listview.java
@@ -2,13 +2,17 @@ package com.example.array;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class Listview extends AppCompatActivity {
 
     String nazioni[] = {"Italia", "Germania", "Olanda", "Stati Uniti"};
+
     ListView countryList;
 
     @Override
@@ -16,8 +20,16 @@ public class Listview extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
 
-        countryList = (ListView) findViewById(R.id.countryList);
-        ArrayAdapter<String> countryArrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, nazioni);
+        countryList = findViewById(R.id.countryList);
+        ArrayAdapter<String> countryArrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, nazioni);
         countryList.setAdapter(countryArrayAdapter);
+
+        countryList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), Display_activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
