ebf12943fe1c1a5d83df42032fcaf7b95ac13c17
Fredd89 <ficofedebla@gmail.com>
2021-12-14 09:52:31 +0100

    Third activity preperly working

--- app/src/main/java/com/example/array/Listview.java
+++ app/src/main/java/com/example/array/Listview.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,23 +12,23 @@ import android.widget.ListView;
 
 public class Listview extends AppCompatActivity {
 
-    String nazioni[] = {"Italia", "Germania", "Olanda", "Stati Uniti"};
+    String film[] = {"The Phantom Menace", "Attack Of The Clones", "Revenge Of The Sith", "Spiderman No Way Home"};
 
-    ListView countryList;
+    ListView filmList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
 
-        countryList = findViewById(R.id.countryList);
-        ArrayAdapter<String> countryArrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, nazioni);
-        countryList.setAdapter(countryArrayAdapter);
+        filmList = findViewById(R.id.filmList);
+        ArrayAdapter<String> countryArrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, film);
+        filmList.setAdapter(countryArrayAdapter);
 
-        countryList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        filmList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), Display_activity.class);
+                Intent intent = new Intent(Listview.this, Display_activity.class);
                 startActivity(intent);
             }
         });
